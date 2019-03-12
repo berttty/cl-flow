134b8de797b43d58e1dd1e7185aa5537c16f0183
berttty <berttycontreras@gmail.com>
2019-03-12 09:34:21 +0300

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { EditorComponent } from './editor/editor.component';
+import { JobsComponent } from './jobs/jobs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    EditorComponent,
+    JobsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FontAwesomeModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,78 @@
+import {Component, OnInit} from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faCoins } from '@fortawesome/free-solid-svg-icons';
+import { faHammer } from '@fortawesome/free-solid-svg-icons';
+import { faBrain } from '@fortawesome/free-solid-svg-icons';
+import { faPuzzlePiece } from '@fortawesome/free-solid-svg-icons';
+import { faBullseye } from '@fortawesome/free-solid-svg-icons';
+import { faChild } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faBars, faCoins, faHammer, faBrain, faPuzzlePiece, faBullseye, faChild);
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css'],
+  animations: [
+    trigger('toggleHeight', [
+      state('close', style({
+        height: '0',
+        display: 'none'
+      })),
+      state('open', style({
+        height: '*',
+        display: 'block',
+        background: 'rgba(0, 0, 0, 0.2)',
+        margin: 0,
+        padding: 0
+      })),
+      transition('close => open',  animate('1000ms ease-in')),
+      transition('open => close', animate('1000ms ease-out'))
+    ])
+  ]
+
+})
+export class MenuComponent implements OnInit {
+  isOpen = false;
+  navigationSubState: { [menu: string]: string} = {
+    sub_1: 'close',
+    sub_2: 'close',
+    sub_1_1: 'close'
+  };
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  menuHeaderAction() {
+    this.isOpen = ! this.isOpen;
+    if ( ! this.isOpen ) {
+      Object
+        .keys(this.navigationSubState)
+        .forEach( (value: string) => {
+          this.closeSubMenu(value);
+        }
+      );
+    }
+    this.isOpen = this.isOpen;
+  }
+
+  toggleNavigationSub(menuName: string) {
+    if (!this.isOpen) {
+      this.menuHeaderAction();
+    }
+    this.navigationSubState[menuName] = (this.navigationSubState[menuName] === 'close' ? 'open' : 'close');
+  }
+
+  closeSubMenu(menuName: string){
+    this.navigationSubState[menuName] =  'close';
+  }
+
+}
