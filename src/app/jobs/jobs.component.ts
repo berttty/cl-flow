6dfad803e26ff685a5f317b2c98c932bf2d5accd
berttty <berttycontreras@gmail.com>
2019-03-12 20:52:03 +0300

    addition node visual part

--- src/app/jobs/jobs.component.ts
+++ src/app/jobs/jobs.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faRunning } from '@fortawesome/free-solid-svg-icons';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { faTasks } from '@fortawesome/free-solid-svg-icons';
+library.add(faCheckCircle, faTasks, faRunning);
 
 @Component({
   selector: 'app-jobs',
--- src/app/node/node.component.ts
+++ src/app/node/node.component.ts
@@ -1,9 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faCoins } from '@fortawesome/free-solid-svg-icons';
+import { faHammer } from '@fortawesome/free-solid-svg-icons';
+import { faBrain } from '@fortawesome/free-solid-svg-icons';
+import { faPuzzlePiece } from '@fortawesome/free-solid-svg-icons';
+import { faBullseye } from '@fortawesome/free-solid-svg-icons';
+import { faChild } from '@fortawesome/free-solid-svg-icons';
 
+library.add(faBars, faCoins, faHammer, faBrain, faPuzzlePiece, faBullseye, faChild);
 @Component({
   selector: 'app-node',
   templateUrl: './node.component.html',
-  styleUrls: ['./node.component.css']
+  styleUrls: [
+    './node.component.css',
+    './node.level1.component.css',
+    './node.level2.ele1.component.css',
+    './node.level2.ele2.component.css',
+    './node.level2.ele3.component.css',
+    './node.level2.ele4.component.css',
+    './node.level2.ele5.component.css'
+  ]
 })
 export class NodeComponent implements OnInit {
 
