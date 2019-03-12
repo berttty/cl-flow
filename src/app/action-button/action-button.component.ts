f84347d2c1f83d9209d2700f0c16a3e0262ec98e
berttty <berttycontreras@gmail.com>
2019-03-12 22:18:14 +0300

    add the action-button visual part

--- src/app/action-button/action-button.component.ts
+++ src/app/action-button/action-button.component.ts
@@ -1,9 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faPlay } from '@fortawesome/free-solid-svg-icons';
+
+
+library.add(faPlay);
 
 @Component({
   selector: 'app-action-button',
   templateUrl: './action-button.component.html',
-  styleUrls: ['./action-button.component.css']
+  styleUrls: [
+    './action-button.component.css',
+    './action-button.save.component.css',
+  ]
 })
 export class ActionButtonComponent implements OnInit {
 
